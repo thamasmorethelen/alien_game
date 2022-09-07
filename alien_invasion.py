01914483ae0cf83c2db4ea46261787bf69f9b74e
Thomas Thelen <thamasthelen@gmail.com>
2022-09-07 11:22:41 -0700

    added main game play loop

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,4 +3,19 @@ import pygame
 
 # class to manage overall behavior of the game
 class AlienInvasion:
-    
+    # Initiates the game and creates game resources
+    pygame.init()
+
+    self.screen = pygame.display.set_mode(1200, 800)
+    pygame.display.set_caption("Alien Invasion")
+
+    # method for main loop of game
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
